038439a6da6ae868655339e84d69841c63ac8617
mohamad mahmudipur <mahmudipur.m@gmail.com>
2024-06-02 07:03:29 +0330

    chore: profile constant added

---
+++ constants/profiles.ts
@@ -0,0 +1,13 @@
+import { TProfile } from "@/utils/Profile";
+
+const person_one: Array<TProfile> = [
+  { label: "نام", value: "محمد", whoCanSee: [0, 1, 2] },
+  { label: "نام خانوادگی", value: "محمودی پور", whoCanSee: [0, 1, 2] },
+  { label: "تاریخ تولد", value: "6 بهمن 1377", whoCanSee: [0, 1] },
+  { label: "محل تولد", value: "مشهد", whoCanSee: [0, 1] },
+  { label: "شماره شناسنامه", value: "0924753359", whoCanSee: [0] },
+  { label: "کد کارگزینی", value: "1029", whoCanSee: [0] },
+  { label: "شناسه بیمه", value: "123456787634", whoCanSee: [0] },
+];
+
+export {person_one}
