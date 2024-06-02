da3420b9e08b55660934acaa54ef9fcf0fca1ed8
mohamad mahmudipur <mahmudipur.m@gmail.com>
2024-06-02 09:54:56 +0330

    fix: :fire: filter nullish profile

--- utils/Profile.ts
+++ utils/Profile.ts
@@ -1,5 +1,3 @@
-import Permission from "./Permissions";
-
 type TProfile = {
   label: string;
   value: string;
@@ -14,17 +12,13 @@ class Profile {
     return this.profile;
   }
   getProfileWithPermission(permissionId: number) {
-    const permissions = new Permission();
     const mappedProfile = this.profile.map((item) => {
       if (item.whoCanSee.includes(permissionId)) {
         return item;
       }
-      const permissionLables = permissions
-        .selectPermissionLables(item.whoCanSee)
-        .join(" و ");
-      return { ...item, value: `قابل نمایش برای ${permissionLables}` };
+      return null
     });
-    return mappedProfile;
+    return mappedProfile.filter(item => item !== null);
   }
 }
 export default Profile;
